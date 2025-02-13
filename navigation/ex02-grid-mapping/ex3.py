7ddb3ae852fa564093538e963fda41f3508e8387
harry-graves <harry.graves@st-annes.ox.ac.uk>
2025-02-13 22:01:33 +0000

    Completed ex02

--- navigation/ex02-grid-mapping/ex3.py
+++ navigation/ex02-grid-mapping/ex3.py
@@ -60,14 +60,58 @@ def bresenham(x0, y0, x1, y1):
     l = np.array(list(bh.bresenham(x0, y0, x1, y1)))
     return l
     
-# def prob2logodds(p):
-    # add code here
-    
-# def logodds2prob(l):
-    # add code here    
-    
-# def inv_sensor_model(cell, endpoint, prob_occ, prob_free):
-    # add code here
+def logodds2prob(logodds):
+    if logodds == float("inf"):
+        return 1
+    else:
+        prob = (10 ** logodds)/(1 + 10 ** logodds)
+        return prob
 
-# def grid_mapping_with_known_poses(ranges_raw, poses_raw, occ_gridmap, map_res, prob_occ, prob_free, prior):
-    # add code here
+def prob2logodds(prob):
+    if prob == 0:
+        return -float("inf")
+    else:
+        logodds = np.log10(prob/(1 - prob))
+        return logodds
+    
+def inv_sensor_model(cell, endpoint, prob_occ, prob_free):
+    if cell == endpoint:
+        return prob_occ  # The laser endpoint is likely occupied
+    else:
+        return prob_free  # Cells along the beam path are more likely free
+    ## I DON'T GET THIS.
+    ## It isn't used in the below function...
+
+def grid_mapping_with_known_poses(ranges_raw, poses_raw, occ_gridmap, map_res, prob_occ, prob_free, prior):
+    """
+    Implements occupancy grid mapping using known poses.
+
+    Parameters:
+    - ranges_raw: Array of sensor measurements (ranges).
+    - poses_raw: Array of robot poses.
+    - occ_gridmap: Initial occupancy grid (log-odds form).
+    - map_res: Resolution of the map.
+    - prob_occ: Probability of occupancy (P(occ)).
+    - prob_free: Probability of free space (P(free)).
+    - prior: Prior probability of occupancy.
+
+    Returns:
+    - Updated occupancy grid (log-odds form).
+    """
+    log_prior = prob2logodds(prior)
+    log_occ = prob2logodds(prob_occ)
+    log_free = prob2logodds(prob_free)
+
+    for t in range(len(poses_raw)):  # Loop over all time steps
+        robot_cell = poses2cells(poses_raw[t], occ_gridmap, map_res)  # Robot position in grid
+        endpoints = ranges2cells(ranges_raw[t], poses_raw[t], occ_gridmap, map_res)  # Sensor measurements in grid
+
+        for endpoint in endpoints.T:
+            free_cells = bresenham(robot_cell[0], robot_cell[1], endpoint[0], endpoint[1])
+
+            for cell in free_cells[:-1]:  # Mark free space
+                occ_gridmap[cell[0], cell[1]] += log_free - log_prior
+
+            occ_gridmap[endpoint[0], endpoint[1]] += log_occ - log_prior  # Mark occupied
+
+    return occ_gridmap

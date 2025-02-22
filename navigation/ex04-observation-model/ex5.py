0e970a3cfe6f06e019629beb32519901b81186a2
harry-graves <harry.graves@st-annes.ox.ac.uk>
2025-02-22 14:22:17 +0000

    Completed ex04-observation-model

--- navigation/ex04-observation-model/ex5.py
+++ navigation/ex04-observation-model/ex5.py
@@ -1,10 +1,22 @@
-# add your fancy code here
+import numpy as np
 
+def prob(mu, sigma):
+
+    # Normal distribution
+    prob = (1 / np.sqrt(2 * np.pi * (sigma**2))) * np.exp(- (mu ** 2) / (2 * (sigma**2)))
+
+    return prob
 
 def landmark_observation_model(z, x, b, sigma_r):
-    # TODO
+    r_hat = np.sqrt((b[0] - x[0])**2 + (b[1] - x[1])**2)
+    #phi_hat = np.arctan2(b[1] - x[1], b[0] - x[0])
+    p = prob(z - r_hat, sigma_r)
     return p
 
 def observation_likelihood(r, b, gridmap, sigma_r, size):
-    # TODO
-    return p
+    
+    # Compute likelihood for each cell in the grid
+    for x, y in np.ndindex(size, size):
+        gridmap[x, y] *= landmark_observation_model(r, [x, y], b, sigma_r)
+
+    return gridmap
